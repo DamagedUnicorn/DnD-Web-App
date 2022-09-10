39748286abb147a3e361f6112257b17518c2c7e6
DamagedUnicorn <tpkzip@hotmail.com>
2022-09-10 22:15:48 +0200

    removed debug option for app.run

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from website import createApp
 app = createApp()
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
